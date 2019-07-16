94826fba6d24274d9eeb762a348f9570a0f38e4d
Austin Cheng <austincheng0@gmail.com>
2019-07-16 16:45:37 -0700

    Cleaner code

--- outlook/Outlook.py
+++ outlook/Outlook.py
@@ -7,7 +7,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.keys import Keys
-import csv
 import os
 import time
 from selenium.webdriver.common.action_chains import ActionChains
@@ -38,12 +37,6 @@ class Outlook:
 
 		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "idSIButton9")))
 		driver.find_element_by_id('idSIButton9').click()
-
-		"""
-		If not on Cisco VPN
-		time.sleep(2)
-		os.system('java Hackathon')
-		"""
 
 		time.sleep(1)
 
@@ -132,3 +125,84 @@ class Outlook:
 
 		driver.close()
 		return userTimes, roomTimes
+
+	@staticmethod
+	def create_event(start, end, location, emails):
+		driver = webdriver.Chrome()
+		driver.get('https://developer.microsoft.com/en-us/graph/graph-explorer')
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ms-signin-button")))
+		driver.find_element_by_id('ms-signin-button').click()
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "i0116")))
+		driver.find_element_by_id('i0116').send_keys('aucheng@cisco.com')
+
+		time.sleep(0.5)
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "idSIButton9")))
+		driver.find_element_by_id('idSIButton9').click()
+
+		time.sleep(1)
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "idSIButton9")))
+		driver.find_element_by_id('idSIButton9').click()
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"explorer-main\"]/request-editors/div/ul/li[2]")))
+		driver.find_element_by_xpath('//*[@id="explorer-main"]/request-editors/div/ul/li[2]').click()
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"headers-editor\"]/table/tbody/tr[2]/td[1]/input")))
+		driver.find_element_by_xpath('//*[@id=\"headers-editor\"]/table/tbody/tr[2]/td[1]/input').send_keys('Content-Type')
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"headers-editor\"]/table/tbody/tr[2]/td[2]/input")))
+		driver.find_element_by_xpath('//*[@id=\"headers-editor\"]/table/tbody/tr[2]/td[2]/input').send_keys('application/json')
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"explorer-main\"]/request-editors/div/ul/li[1]")))
+		driver.find_element_by_xpath('//*[@id=\"explorer-main\"]/request-editors/div/ul/li[1]').click()
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"graph-request-url\"]/input")))
+		driver.find_element_by_xpath('//*[@id=\"graph-request-url\"]/input').clear()
+		driver.find_element_by_xpath('//*[@id=\"graph-request-url\"]/input').send_keys('https://graph.microsoft.com/v1.0/me/calendar/events')
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"post-body-editor\"]/div[2]/div")))
+		div = driver.find_element_by_xpath('//*[@id=\"post-body-editor\"]/div[2]/div')
+		attendees = '['
+		for i in range(len(emails)):
+			email = emails[i]
+			attendees += Outlook.email_box(email)
+			if i != len(email) - 1:
+				attendees += ', '
+		attendees += ']'
+
+		actions = ActionChains(driver)
+		actions.move_to_element(div)
+		actions.click(div)
+		actions.send_keys('{\"subject\": \"Hackathon Demo Meet\",\"body\": {\"contentType\": \"HTML\",\"content\": \"Let\'s sync up for the Schedder hackathon demo.\"},' + \
+								'\"start\": {' + \
+									'\"dateTime\": \"' + start + '\",' + \
+									'\"timeZone\": \"Pacific Standard Time\"' + \
+								'},' + \
+								'\"end\": {' + \
+									'\"dateTime\": \"' + end + '\",' + \
+									'\"timeZone\": \"Pacific Standard Time\"' + \
+								'},' + \
+								'\"location\":{' + \
+									'\"displayName\":\"' + location + '\"' + \
+								'},' + \
+								'\"attendees\":' + attendees + \
+							'}')
+		actions.perform()
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"httpMethodSelect\"]/div/button")))
+		driver.find_element_by_xpath('//*[@id=\"httpMethodSelect\"]/div/button').click()
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "httpMethodSelect-select-POST")))
+		driver.find_element_by_id('httpMethodSelect-select-POST').click()
+
+		WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "submitBtn")))
+		driver.find_element_by_id('submitBtn').click()
+
+		driver.close()
+
+	@staticmethod
+	def email_box(email):
+		return "{{\"emailAddress\": {{ \"address\":\"{}\"}},\"type\": \"required\"}}".format(email)
